4a8bb0fa7ed9463f16bc643043e1bec5e2632f84
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-04-21 18:06:43 -0400

    text to test

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <html>
 <body>
   <h1>This is a login page</h1>
-  <form method = "post" action = "index.php">
-  <strong>username:</strong><input type = "text" name="reg_uname" value=""/><br><br>
+  <form method="post" action="index.php" >
+  <strong>username:</strong><input type="test" name="reg_uname" value=""/><br><br>
   <strong>Password:</strong><input type="password" name="reg_password" value=""/><br><br>
   <input type="hidden" name="action" value="test_user"/><br>
-  <input type="submit" value="login"/>
+  <input type="submit" value="Login"/>
   </form>
   <form action="register.php">
-	<input type="submit" value="Register">
+	<input type="submit" value="Register" />
   </form>
 </body>
 </html>
