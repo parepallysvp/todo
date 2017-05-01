95cbabfa4ebeb010f62789fadf01512cc9d919ad
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-05-01 19:21:15 -0400

    isUserValid function corrected

--- db.php
+++ db.php
@@ -55,24 +55,24 @@ function createUser($username, $password){
 }
 function isUserValid($username, $password){
   global $db;
-  $query = 'select * from users where username = :name and psswordHash = :pass';
+  $query = 'select * from users where username = :name and passwordHash = :pass';
   $statement = $db->prepare($query);
   $statement->bindValue(':name',$username);
   $statement->bindValue(':pass',$password);
   $statement->execute();
-  $result=$statement->fetchAll();
+  $result=  $statement->fetchAll();
   $statement->closeCursor();
   $count=$statement->rowCount();
   if($count == 1){
        setcookie('login', $username);
        setcookie('my_id', $result[0]['id']);
-       setcookie('isLogged', true);
+       setcookie('islogged', true);
        return true;
      }else{
        unset($_COOKIE['login']);
        setcookie('login', false);
-       setcookie('isLogged', false);
-       setcookie('my_id', false);
+       setcookie('islogged', false);
+       setcookie('id', false);
        return false;
      }
 }
