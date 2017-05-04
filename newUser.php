9be6743cb50f096ea8895620abf7d212c062c85d
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-05-04 00:18:36 -0400

    new file

---
+++ newUser.php
@@ -0,0 +1,11 @@
+<?php
+echo "Welcome " . $_POST['reg_username'] . ",  your account has been created. Now you can log in";
+?>
+<html>
+<body>
+<form action="login.php">
+<input type="submit" value="login page" />
+</form>
+</body>
+</html>
+
