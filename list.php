ef6c58ac4d7fd9eb5717e2bb77de3763653946c8
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-05-05 23:49:37 -0400

    added new table columns and new functionality to add form

--- list.php
+++ list.php
@@ -3,15 +3,20 @@
 
 </head>
 <body>
+	<form align="right" action="login.php">
+	<input type="submit" value="log out">
+	</form>
 	<h1> To do list system</h1>
 	<h3> Welcome <?php echo $_COOKIE['fname'].", ".$_COOKIE['lname']; ?></h3><br/>
 	<p> Below, you may find your to-do items </p><br/> 
   <table>
 	<?php foreach($result as $res):?>
 	<tr>
-	   <td><a href='detail.php'><?php echo $res['todo_item']. '<br />';?></a></td>
-	   <td>
-	     <form action='index.php' method='post'>
+	<td><a href='detail.php'><?php echo $res['todo_item']. '<br />';?></a></td>
+	<td><?php echo $res['date']. '<br/>'; ?></td>
+	<td><?php echo $res['time']. '<br/>'; ?></td>
+	<td>
+	<form action='index.php' method='post'>
 	<input type='hidden' name='item_id' value='<?php echo $res['id']  ?>'>
 	<input type='hidden' name='action' value='delete'>
 	<input type='submit' value="delete"></form>
@@ -19,16 +24,9 @@
 	</tr>
 	  <?php endforeach; ?>
 
-	<!-- <tr>
-	 <form action="add_todo.php">
-	  <input type="submit" value="Add"/>
-	 </form>
-	</tr>-->
 	</table>
-	<form method ='post' action='index.php'>
-	<span> <strong> Description: </strong> <input type='text' name='description'/>
-	<input type='hidden' name='action' value='add'>
-	<input type="submit" value="Add"/></span>
+	<form method ='post' action='addform.php'>
+	<span><label> Add a new item</label><input type="submit" value="Add"/></span>
 </form> 
  </body>
 </html> 
