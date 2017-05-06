a55a3974595ba3d118a83141a532e85ede0d001e
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-05-06 13:31:48 -0400

    header and footer

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> ToDo's Inc.
+    </p>
+</footer>
+</body>
+</html>
+
---
+++ view/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>ToDo's</title>
+    <link rel="stylesheet" type="text/css" href="./css/basic.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Yellowtail" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Holtwood+One+SC" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merienda+One" />
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>ToDo</h1></header>
+
+
