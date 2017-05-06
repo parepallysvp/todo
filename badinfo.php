56744df677aba1c94e2dfc9a5e38436e7d87ee14
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-05-05 23:48:43 -0400

    modified

--- badinfo.php
+++ badinfo.php
@@ -1,5 +1,10 @@
-<?php
-  echo "wrong username and password please try again? <br/>";
-?>
+<html>
+<body>
+	<h4>Wrong username, Please try again by entering the correct username</h4><br/>
+	<form action="login.php">
+	<input type="submit" value="login">
+	</form>
+</body>
+</html>
 
 
