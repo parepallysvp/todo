f9c39981484508337ae73199d1741e8a48e302a1
sai vara prathyush parepally <sp2257@afsconnect2.njit.edu>
2017-05-07 10:27:34 -0400

    added css elements

--- view/header.php
+++ view/header.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
 <html>
-<!-- the head section -->
 <head>
     <title>ToDo's</title>
-    <link rel="stylesheet" type="text/css" href="./css/basic.css">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Yellowtail" />
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Holtwood+One+SC" />
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merienda+One" />
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Aclonica" />
 </head>
-
-<!-- the body section -->
-<body>
-<header><h1>ToDo</h1></header>
-
+</html
 
